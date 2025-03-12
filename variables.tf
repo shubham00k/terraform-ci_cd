afc99b3c84aa265b239262158e23cbef0f45e7ef
shubham00k <shubhamkapse193@gmail.com>
2025-03-12 22:14:28 +0530

    Updated Terraform provider to use GitHub Secrets

--- variables.tf
+++ variables.tf
@@ -14,3 +14,15 @@ variable "s3_bucket_name" {
     default = "my-bucketwithwater-123456"
   
 }
+
+variable "AWS_ACCESS_KEY_ID" {
+  description = "AWS Access Key ID"
+  type        = string
+  sensitive   = true
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  description = "AWS Secret Access Key"
+  type        = string
+  sensitive   = true
+}
